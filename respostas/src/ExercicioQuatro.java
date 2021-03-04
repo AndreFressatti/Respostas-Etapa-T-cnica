ae0a0e6f1825843edb946e76571d7bbc8d5e139f
unknown <fressatti2003@gmail.comgit config --global user.email fressatti2003@gmail.com>
2021-03-04 15:40:22 -0300

    exercicios 1,2,4

---
+++ respostas/src/ExercicioQuatro.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExercicioQuatro {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int tamanho = scanner.nextInt();
+
+		for (int i = 0; i < tamanho; i++) {
+			System.out.print("Incomoda ");
+		}
+
+		System.out.print("muito mais.");
+
+	}
+}
